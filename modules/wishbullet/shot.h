01e0081a2a7dea4994f2bcd52902f8fa3023e693
WishMakers <WishMakers0@github.com>
2021-01-13 19:08:23 -0500

    Start on wishbullet module

---
+++ modules/wishbullet/shot.h
@@ -0,0 +1,21 @@
+#ifndef SHOT_H
+#define SHOT_H
+
+#include "core/object.h"
+#include "core/reference.h"
+#include "bullet_type.h"
+#include "core/math/transform_2d.h"
+#include "core/math/rect2.h"
+#include "scene/resources/texture.h"
+
+class Shot : public Object {
+
+	protected:
+		
+	
+	public:
+		
+	
+}
+
+#endif
